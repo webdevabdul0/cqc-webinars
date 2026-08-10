d2ad9c74d28a9023a486ebd604458c5834af973e
Abdul Hanan <webdevabdul@gmail.com>
2026-08-10 12:29:21 +0500

    Restore NextUp, HowItWorks, Replays and Band sections to webinar page

--- src/pages/WebinarsPage.jsx
+++ src/pages/WebinarsPage.jsx
@@ -1,11 +1,19 @@
 import Hero from '../components/Hero'
+import NextUp from '../components/NextUp'
 import UpcomingWebinars from '../components/UpcomingWebinars'
+import HowItWorks from '../components/HowItWorks'
+import Replays from '../components/Replays'
+import Band from '../components/Band'
 
 export default function WebinarsPage({ onRegister }) {
   return (
     <>
       <Hero onRegister={onRegister} />
+      <NextUp onRegister={onRegister} />
       <UpcomingWebinars onRegister={onRegister} />
+      <HowItWorks />
+      <Replays onRegister={onRegister} />
+      <Band />
     </>
   )
 }
